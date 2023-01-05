09ef668f9fdf1e32113eb3318929097986046d1c
Haikal Danung Trinanda <77687182+hkldtrinanda@users.noreply.github.com>
2023-01-05 20:40:27 +0700

    Initial commit

---
+++ Assets/ChangeSceneScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneScript : MonoBehaviour
+{
+    public string sceneName, sceneName2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void GoBackToMenuMateri()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+    public void GoToBeranda()
+    {
+        SceneManager.LoadScene(sceneName2);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    
+}
